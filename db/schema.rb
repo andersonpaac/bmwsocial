1300e2bbc1a07f81b78cdf5288172ce4e63dfa00
Li Roy <qtb8985@ww10sc2353040.americas.bmw.corp>
2016-04-04 16:27:19 -0500

    added destination model & association between destinations and actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404211821) do
+ActiveRecord::Schema.define(version: 20160404212357) do
 
   create_table "actions", force: :cascade do |t|
     t.string   "type"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160404211821) do
     t.string  "address"
     t.decimal "distance"
     t.string  "estimate_time"
+    t.integer "action_id"
   end
 
   create_table "users", force: :cascade do |t|
