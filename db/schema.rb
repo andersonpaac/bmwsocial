f4d70e72b0a3c3aeeb5da9a7ed6d56ed8dc40423
Li Roy <qtb8985@ww10sc2353040.americas.bmw.corp>
2016-04-04 16:21:14 -0500

    added migration for destinations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404201755) do
+ActiveRecord::Schema.define(version: 20160404211821) do
 
   create_table "actions", force: :cascade do |t|
     t.string   "type"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160404201755) do
   end
 
   add_index "actions", ["social_username"], name: "index_actions_on_social_username", unique: true
+
+  create_table "destinations", force: :cascade do |t|
+    t.string  "name"
+    t.string  "address"
+    t.decimal "distance"
+    t.string  "estimate_time"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
